a5abfab19c9aef426b1e3f67ba18f144cc863920
Olga Vaikul <olenka1201@list.ru>
2023-04-11 08:20:53 +0300

    complete task

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,11 +23,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  let matrix1 = [];
+  for(let i=0; i<matrix.length; i++){
+    for (let j=0; j<matrix[i].length; j++){
+      if(matrix[i][j] === true) {
+        matrix1.push([i, j])
+      }
+    }
+    return matrix1;
 }
-
+}
 module.exports = {
   minesweeper
 };
