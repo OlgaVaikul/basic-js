e39a85df2cd4161e24a103513b95d595b2e97759
Olga Vaikul <olenka1201@list.ru>
2023-04-17 18:56:43 +0300

    complete task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,11 +17,12 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity === "undefined") return false;
+  if (typeof sampleActivity === null) return false;
+  if (typeof sampleActivity === "array") return false;
+  if (typeof sampleActivity === "object") return false;
 }
-
 module.exports = {
   dateSample
 };
